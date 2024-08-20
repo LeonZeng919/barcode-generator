4e4adba290b32660de9133427ba350f0e12326fc
Leon Zeng <ncuzengliang@163.com>
2024-08-20 19:22:06 +0800

    update css

--- src/components/barcode-generator/index.tsx
+++ src/components/barcode-generator/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { BarcodeProvider } from './BarcodeContext'
 import { BarcodeCarousel } from './BarcodeCarousel'
 import {
@@ -22,31 +22,19 @@ import { Menu, Sliders } from 'lucide-react'
 const BarcodeGeneratorContent: React.FC = () => {
   useBarcodeGenerator()
 
-  const [isSmallScreen, setIsSmallScreen] = useState(false)
   const [activePanel, setActivePanel] = useState<
     'main' | 'carousel' | 'options'
   >('main')
   const [isLeftPanelCollapsed, setIsLeftPanelCollapsed] = useState(false)
   const defaultLayout = [20, 50, 30]
   const navCollapsedSize = 4
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth < 1024)
-    }
-
-    handleResize()
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-
   const togglePanel = (panel: 'carousel' | 'options') => {
     setActivePanel(activePanel === panel ? 'main' : panel)
   }
 
-  if (isSmallScreen) {
-    return (
-      <div className="flex h-full max-h-[800px] flex-col">
+  return (
+    <div className="barcode-generator-content">
+      <div className="small-screen-layout">
         <div className="flex items-center justify-between p-2">
           <Button
             onClick={() => togglePanel('carousel')}
@@ -64,70 +52,72 @@ const BarcodeGeneratorContent: React.FC = () => {
           </Button>
         </div>
         <div className="flex-grow overflow-auto">
-          {activePanel === 'main' && (
+          <div className={`panel ${activePanel === 'main' ? 'active' : ''}`}>
             <div className="flex flex-col gap-4 p-4">
               <InputComponent />
               <OutputComponent />
               <DownloadBarcodes />
             </div>
-          )}
-          {activePanel === 'carousel' && (
+          </div>
+          <div
+            className={`panel ${activePanel === 'carousel' ? 'active' : ''}`}
+          >
             <div className="p-4">
               <BarcodeCarousel isCollapsed={false} />
             </div>
-          )}
-          {activePanel === 'options' && (
+          </div>
+          <div className={`panel ${activePanel === 'options' ? 'active' : ''}`}>
             <div className="p-4">
               <OptionsComponent />
             </div>
-          )}
+          </div>
         </div>
       </div>
-    )
-  }
 
-  return (
-    <ResizablePanelGroup
-      direction="horizontal"
-      className="h-full max-h-[800px] items-stretch"
-    >
-      <ResizablePanel
-        defaultSize={defaultLayout[0]}
-        collapsedSize={navCollapsedSize}
-        collapsible={true}
-        minSize={5}
-        maxSize={22}
-        onCollapse={() => setIsLeftPanelCollapsed(true)}
-        onExpand={() => setIsLeftPanelCollapsed(false)}
-      >
-        <div className="p-4">
-          <BarcodeCarousel isCollapsed={isLeftPanelCollapsed} />
-        </div>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
-        <div className="flex flex-col gap-4 p-4">
-          <div>
-            <InputComponent />
-          </div>
-          <div>
-            <OutputComponent />
-          </div>
-        </div>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={defaultLayout[2]} minSize={30}>
-        <div className="flex h-full flex-col p-4">
-          <div className="">
-            <OptionsComponent />
-          </div>
-          <div className="">
-            <Separator className="my-4" />
-            <DownloadBarcodes />
-          </div>
-        </div>
-      </ResizablePanel>
-    </ResizablePanelGroup>
+      <div className="large-screen-layout">
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="h-full max-h-[800px] items-stretch"
+        >
+          <ResizablePanel
+            defaultSize={defaultLayout[0]}
+            collapsedSize={navCollapsedSize}
+            collapsible={true}
+            minSize={5}
+            maxSize={22}
+            onCollapse={() => setIsLeftPanelCollapsed(true)}
+            onExpand={() => setIsLeftPanelCollapsed(false)}
+          >
+            <div className="p-4">
+              <BarcodeCarousel isCollapsed={isLeftPanelCollapsed} />
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
+            <div className="flex flex-col gap-4 p-4">
+              <div>
+                <InputComponent />
+              </div>
+              <div>
+                <OutputComponent />
+              </div>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={defaultLayout[2]} minSize={30}>
+            <div className="flex h-full flex-col p-4">
+              <div className="">
+                <OptionsComponent />
+              </div>
+              <div className="">
+                <Separator className="my-4" />
+                <DownloadBarcodes />
+              </div>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+    </div>
   )
 }
 
